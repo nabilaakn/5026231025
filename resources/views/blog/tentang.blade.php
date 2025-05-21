0fdc5353a00382b6c59537df27847a9c554992ca
nabilaakn <nabilarahadatul16@gmail.com>
2025-05-21 12:34:56 +0700

    pertemuan 12

---
+++ resources/views/blog/tentang.blade.php
@@ -0,0 +1,13 @@
+<!-- Menghubungkan dengan view template master -->
+@extends('blog/master')
+@section('title','Teknik Template')
+@section('judul_halaman', 'Halaman Tentang')
+
+@section('konten')
+
+	<p>Ini Adalah Halaman Tentang</p>
+	<p>
+	tentang nala, dan hati yang sedang berbunga
+	</p>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Link;
+use App\Http\Controllers\BlogController;
 
 /*
 |--------------------------------------------------------------------------
@@ -43,3 +45,13 @@ Route::get('ETS', function () {
 Route::get('frontend', function () {
 	return view('frontend');
 });
+Route::get('dosen', [Link::class,'index']);
+Route::get('/pegawai/{nama}', [PegawaiController::class,'index']);
+
+Route::get('/formulir', [PegawaiController::class,'formulir']);
+Route::post('/formulir/proses', [PegawaiController::class,'proses']);
+
+//route blog
+Route::get('/blog', [BlogController::class, 'home']);
+Route::get('/blog/tentang', [BlogController::class, 'tentang']);
+Route::get('/blog/kontak', [BlogController::class, 'kontak']);
